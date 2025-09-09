305cee32b79b1ac702841e4cdc6902c62bbc9d1d
JohannesSchaefers <info@iconicity.de>
2025-09-09 23:14:01 +0200

    index aus genau

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+/*
 import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
@@ -20,3 +21,21 @@ export const handler: Handlers = {
     });
   },
 };
+*/
+
+// routes/index.tsx
+
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  GET(_req) {
+    return new Response(null, {
+      status: 303, // Consistent with middleware.ts
+      headers: { Location: "/login" },
+    });
+  },
+};
+
+export default function Home() {
+  return null; // No content needed, as it redirects
+}
